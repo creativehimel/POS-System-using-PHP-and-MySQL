f64c20125f8422b4ac7b6ac02568b7f281555aba
creativehimel <himel.phy@bsmrstu.edu.bd>
2024-01-08 01:48:48 +0600

    Create a custom function for CRUD SQL Query

--- admin/config/function.php
+++ admin/config/function.php
@@ -27,6 +27,89 @@ function alertMessage(){
     }
 }
 
+// Insert record in database using this function
+function insertRecord($tableName, $data){
+    global $conn;
+    $table = validate($tableName);
+    $columns = array_keys($data);
+    $values = array_values($data);
+    $finalColumn = implode(',', $columns);
+    $finalvalue = "'".implode("', '", $values)."'";
+    $query = "INSERT INTO $table ($finalColumn) VALUES ($finalvalue)";
+    $result = mysqli_query($conn, $query);
+    return $result;
+}
 
+//Update record in database using this function
+
+function updateRecord($tableName, $id, $data){
+    global $conn;
+    $table = validate($tableName);
+    $id = validate($id);
+    $updateDataString = "";
+    foreach($data as $column => $value){
+        $updateDataString .= $column.'='."'$value',";
+    }
+    $finalUpdateData = substr(trim($updateDataString),0,-1);
+    $query = "UPDATE $table SET $finalUpdateData WHERE id='$id'";
+    $result = mysqli_query($conn, $query);
+    return $result;
+}
+
+// Get All records using this function
+function getAllRecord($tableName, $status= NULL){
+    global $conn;
+    $table = validate($tableName);
+    $status = validate($status);
+    if($status == 'status'){
+        $query = "SELECT * FROM $table WHERE status = '0'";
+    }else{
+        $query = "SELECT * FROM $table";
+    }
+    return mysqli_query($conn, $query);
+}
+
+// Get Record based on id using this function
+function getRecordById($tableName, $id){
+    global $conn;
+    $table = validate($tableName);
+    $id = validate($id);
+    $query = "SELECT * FROM $table WHERE id = '$id' LIMIT 1";
+    $result = mysqli_query($conn, $query);
+    if($result){
+        if(mysqli_num_rows($result) == 1){
+            $row = mysqli_fetch_assoc($result);
+            $response = [
+            'status' => 200,
+            'data' => $row,
+            'message' => 'Record Found.'
+            ];
+            return $response;
+            
+        }else{
+            $response = [
+            'status' => 404,
+            'message' => 'No Record Found.'
+            ];
+            return $response;
+        }
+    }else{
+        $response = [
+            'status' => 500,
+            'message' => 'Something went wrong'
+        ];
+        return $response;
+    }
+}
+
+// Delete Record from database using this function
+function deleteRecord($tableName, $id){
+    global $conn;
+    $table = validate($tableName);
+    $id = validate($id);
+    $query = "DELETE FROM $table WHERE id='$id' LIMIT 1";
+    $result = mysqli_query($conn, $query);
+    return $result;
+}
 
 ?>
