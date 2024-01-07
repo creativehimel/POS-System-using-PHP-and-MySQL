28f9a7c5afb9bf8de28bbebf8ec0f66041a4f0c5
creativehimel <himel.phy@bsmrstu.edu.bd>
2024-01-08 00:03:47 +0600

    Project initialized

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer class="container">
+    <section class="d-flex justify-content-between">
+        <div>
+            <p>2024 © Creative POS.</p>
+        </div>
+        <div>
+            <p>Design & Develop by Md. Himel Ali</p>
+        </div>
+    </section>
+
+</footer>
